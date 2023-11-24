701295a0def9563afd643ed2a32d3783d98910f7
naomiinyele <inyeleinyele@gmail.com>
2023-11-24 03:49:45 +0300

    Assignment

---
+++ lib/app.py
@@ -0,0 +1,11 @@
+# lib/app.py
+#print("Hello world!")
+#print("Hello world!", end=" ")
+#print("Hello sun!", end="!! ")
+#print("Hello sky!", end="!!!\n")
+
+def test_app_py_exists():
+     print( "Hello World! Pass this test, please.")
+     return test_app_py_exists
+test_app_py_exists()
+    
